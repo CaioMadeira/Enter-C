be2f3547a0d2e0331d622bf2248f9d6b5e26cef5
Caio Madeira <51931284+CaioMadeira@users.noreply.github.com>
2022-06-20 23:38:01 -0300

    clear

--- test/HelloAgainWorld/HelloAgainWorld/HelloAgainWorld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-	printf("Hello verbose my old friend...");
-
-}
--- test/playground1/playground1/playground1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-	long int x = 5;
-	double y = 7.0;
-
-	printf("\t %d,  \t %e", x, y);
-}
